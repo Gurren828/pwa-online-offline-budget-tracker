cfa69534614d049b994dcd26abe74f88613ccf39
Ruben Burrell <rubenburrell828@gmail.com>
2020-05-05 20:57:22 -0700

    repaired code after pushing damaged code from wrong terminal in vscode

---
+++ public/service-worker.js
@@ -0,0 +1,86 @@
+// TODO: implement service worker so that users can use the app offline. The SW
+// will need to cache static assets to display the app offline. Additionally,
+// the SW should cache transaction data, using the cached data as a fallback
+// when the app is used offline. HINT: You should use two caches. One for the
+// static assets such ass html, css, js, images, etc; and another cache for
+// the dynamic data from requests to routes beginning with "/api".
+
+const FILES_TO_CACHE = [
+    "/", 
+    "/db.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css"
+  ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+    evt.respondWith(
+      fetch(evt.request).catch(function(){
+        return caches.match(evt.request).then(function(response){
+          if (response){
+            return response; 
+          } else if (evt.request.header.get ("accept").includes("text/html")){
+            return caches.match("/");
+          }
+        })
+      })
+    )
+});
